969ff7edd090d5de7cceb2f1c8f0cc467f679465
jgraeb <jgraeben@caltech.edu>
2021-09-17 16:41:58 +0200

    add simple abstraction example

--- winning_set.py
+++ winning_set.py
@@ -20,7 +20,7 @@ import os
 from omega.games import enumeration as enum
 from omega.symbolic import enumeration as sym_enum
 from copy import deepcopy
-import pdb
+import ipdb
 from omega.games import gr1
 from omega.logic import syntax as stx
 from omega.symbolic import fixpoint as fx
@@ -165,7 +165,7 @@ class WinningSet:
         # test
         # vrs = {'x=3'}
         # state_dict = {var: stx.prime(var) for var in vrs}
-        st()
+        # st()
         check_bdd = aut.let(state_dict, z)
         if check_bdd == aut.true:
             return True
@@ -366,8 +366,170 @@ def example_win_set():
     specs.qinit = r'\E \A'
     return specs
 
+def test_spec():
+    sys_vars = {}
+    sys_vars['x'] = (1, 3)
+    sys_vars['y'] = (1,2)
+    sys_init = {'x='+str(1), 'y='+str(1)}
+    sys_prog = {'y=2'} # Eventually merge
+    sys_safe = set()
+    # Dynamics
+    sys_safe |= {'(x=1 && y=1) -> X((x=1 && y=1) && (x=2 && y=1) && (x=2 && y=2))'}
+    sys_safe |= {'(x=2 && y=1) -> X((x=2 && y=1) && (x=3 && y=1) && (x=3 && y=2))'}
+    sys_safe |= {'(x=3 && y=1) -> X(x=3 && y=1)'}
+    sys_safe |= {'(x=1 && y=2) -> X((x=1 && y=2) && (x=2 && y=2) && (x=2 && y=1))'}
+    sys_safe |= {'(x=2 && y=2) -> X((x=2 && y=2) && (x=3 && y=2) && (x=3 && y=1))'}
+    sys_safe |= {'(x=3 && y=2) -> X(x=3 && y=2)'}
+    # Testers
+    tester_vars = {}
+    tester_vars['x1'] = (1,3)
+    tester_vars['y1'] = (1,2)
+    tester_vars['x2'] = (1,3)
+    tester_vars['y2'] = (1,2)
+    tester_init = {'x1='+str(2), 'y1='+str(2), 'x2='+str(1), 'y2='+str(2)}
+    tester_prog = set() # for now everything is ok
+    tester_prog = {'(y1=2 && y2=2)'}
+    tester_safe = set()
+    # no collision
+    for ii in range(1,3+1):
+        for jj in range(1,2+1):
+            sys_safe |= {'!(x='+str(ii)+' && x1 ='+str(ii)+' && y= '+str(jj)+ ' && y1 = '+str(jj)+')'}
+            sys_safe |= {'!(x='+str(ii)+' && x2 ='+str(ii)+' && y= '+str(jj)+ ' && y2 = '+str(jj)+')'}
+            tester_safe |= {'!(x1='+str(ii)+' && x2 ='+str(ii)+' && y1= '+str(jj)+ ' && y2 = '+str(jj)+')'}
+    # Dynamics
+    tester_safe |= {'(x1=1 && y1=2) -> X((x1=1 && y1=2) && (x1=2 && y1=2))'}
+    tester_safe |= {'(x1=2 && y1=2) -> X((x1=2 && y1=2) && (x1=3 && y1=2))'}
+    tester_safe |= {'(x1=3 && y1=2) -> X(x1=3 && y1=2)'}
+    tester_safe |= {'(x2=1 && y2=2) -> X((x2=1 && y2=2) && (x2=2 && y2=2))'}
+    tester_safe |= {'(x2=2 && y2=2) -> X((x2=2 && y2=2) && (x2=3 && y2=2))'}
+    tester_safe |= {'(x2=3 && y2=2) -> X(x2=3 && y2=2)'}
+    # Synthesize specs
+    ego_spec = Spec(sys_vars, sys_init, sys_safe, sys_prog)
+    test_spec = Spec(tester_vars, tester_init, tester_safe, tester_prog)
+    # st()
+
+    return ego_spec, test_spec
+
+def spec_merge_in_front():
+    sys_vars = {}
+    sys_vars['x'] = (1, 3)
+    sys_vars['y'] = (1,2)
+    sys_init = {'x='+str(1), 'y='+str(1)}
+    sys_prog = {'y=2'} # Eventually merge
+    sys_safe = set()
+    # Dynamics
+    sys_safe |= {'(x=1 && y=1) -> X((x=1 && y=1) && (x=2 && y=1) && (x=2 && y=2))'}
+    sys_safe |= {'(x=2 && y=1) -> X((x=2 && y=1) && (x=3 && y=1) && (x=3 && y=2))'}
+    sys_safe |= {'(x=3 && y=1) -> X(x=3 && y=1)'}
+    sys_safe |= {'(x=1 && y=2) -> X((x=1 && y=2) && (x=2 && y=2) && (x=2 && y=1))'}
+    sys_safe |= {'(x=2 && y=2) -> X((x=2 && y=2) && (x=3 && y=2) && (x=3 && y=1))'}
+    sys_safe |= {'(x=3 && y=2) -> X(x=3 && y=2)'}
+    # Testers
+    tester_vars = {}
+    tester_vars['x1'] = (1,3)
+    # tester_vars['y1'] = (1,2)
+    tester_init = {'x1='+str(1)}#, 'y1='+str(2), 'x2='+str(1), 'y2='+str(2)}
+    tester_prog = set() # for now everything is ok
+    # tester_prog = {'(y=2)'}
+    tester_safe = set()
+    # no collision
+    for ii in range(1,3+1):
+        for jj in range(1,2+1):
+            sys_safe |= {'!(x='+str(ii)+' && x1 ='+str(ii)+' && y= '+str(jj)+')'}
+            tester_safe |= {'!(x='+str(ii)+' && x1 ='+str(ii)+' && y= '+str(jj)+')'}
+            # sys_safe |= {'!(x='+str(ii)+' && x2 ='+str(ii)+' && y= '+str(jj)+ ' && y2 = '+str(jj)+')'}
+            # tester_safe |= {'!(x1='+str(ii)+' && x2 ='+str(ii)+' && y1= '+str(jj)+ ' && y2 = '+str(jj)+')'}
+    # Dynamics
+    tester_safe |= {'(x1=1) -> X((x1=1) && (x1=2))'}
+    tester_safe |= {'(x1=2) -> X((x1=2) && (x1=3))'}
+    tester_safe |= {'(x1=3) -> X(x1=3)'}
+    # tester_safe |= {'(x2=1 && y2=2) -> X((x2=1 && y2=2) && (x2=2 && y2=2))'}
+    # tester_safe |= {'(x2=2 && y2=2) -> X((x2=2 && y2=2) && (x2=3 && y2=2))'}
+    # tester_safe |= {'(x2=3 && y2=2) -> X(x2=3 && y2=2)'}
+    # Synthesize specs
+    ego_spec = Spec(sys_vars, sys_init, sys_safe, sys_prog)
+    test_spec = Spec(tester_vars, tester_init, tester_safe, tester_prog)
+    # st()
+    return ego_spec, test_spec
+
+# System and environment have been switched:
+def specs_for_entire_track_vMod(tracklength):
+    # st()
+    sys_vars = {}
+    sys_vars['x'] = (1, tracklength) # 0: system is much behind the second tester car, 6: system is much further than the first tester car
+    sys_vars['y'] = (1,2)
+    sys_init = {'x='+str(1), 'y='+str(1)}
+    sys_prog = {'y=2'} # Eventually, the system should merge
+    sys_safe = set()
+
+    # Dynamics for merging into adjacent track:
+    for ii in range(1,tracklength):
+        sys_safe |= {'(x='+str(ii)+' && y=1) -> X((x='+str(ii+1)+' && y=1)||(x='+str(ii)+' && y=1)|| (x='+str(ii+1)+' && y=2))'}
+        sys_safe |= {'(x='+str(ii)+' && y=2) -> X((x='+str(ii+1)+' && y=2)||(x='+str(ii)+' && y=2)|| (x='+str(ii+1)+' && y=1))'}
+    sys_safe |= {'(x='+str(tracklength)+' && y=1) -> X(x='+str(tracklength)+' && y=1)'}
+    sys_safe |= {'(x='+str(tracklength)+' && y=2) -> X(x='+str(tracklength)+' && y=2)'}
+    # sys_safe |= {'x=0 -> X(x=0 && x=1)'}
+    # sys_safe |= {'x='+str(tracklength-1)+' -> X(x='+str(tracklength-1)+' && x='+str(tracklength)+')'}
+    # sys_safe |= {'x='+str(tracklength)+'-> X(x='+str(tracklength)+')'}
+
+    # testers
+    tester_vars = {}
+    tester_vars['x1'] = (2,tracklength)
+    tester_vars['y1'] = (1,2)
+    tester_vars['x2'] = (1,tracklength-1)
+    tester_vars['y2'] = (1,2)
+    tester_init = {'x1='+str(2), 'y1='+str(2), 'x2='+str(1), 'y2='+str(2)}
+    tester_prog = set()
+    # for ki in range(1,tracklength-1):
+    #     tester_prog |= {'((x='+str(ki+1)+') && (x1='+str(ki+2)+') && (x2='+str(ki)+')) && (y=2 && y1=2 && y2=2)'}
+    # tester_prog |= {'(y=2 && y1=2 && y2=2)'}
+    tester_safe = set()
+
+    # No collision with other vehicles:
+    for yi in range(1,3):
+        for xi in range(1, tracklength+1):
+            if xi!= tracklength:
+                tester_safe |= {'!(x1='+str(xi)+' && x2 ='+str(xi)+' && y1= '+str(yi)+ ' && y2 = '+str(yi)+')'}
+                tester_safe |= {'!(x='+str(xi)+' && x2 ='+str(xi)+' && y= '+str(yi)+ ' && y2 = '+str(yi)+')'}
+                sys_safe |= {'!(x='+str(xi)+' && x2 ='+str(xi)+' && y= '+str(yi)+ ' && y2 = '+str(yi)+')'}
+
+            if xi != 1:
+                tester_safe |= {'!(x1='+str(xi)+' && x2 ='+str(xi)+' && y1= '+str(yi)+ ' && y2 = '+str(yi)+')'}
+                tester_safe |= {'!(x='+str(xi)+' && x1 ='+str(xi)+' && y= '+str(yi)+ ' && y1 = '+str(yi)+')'}
+                sys_safe |= {'!(x='+str(xi)+' && x1 ='+str(xi)+' && y= '+str(yi)+ ' && y1 = '+str(yi)+')'}
+
+    tester_safe |= {'!(y1=1) && !(y2=1)'} # testers stay in bottom lane
+
+    # Tester dynamics
+    for ii in range(1,tracklength):
+        # tester_safe |= {'(x1='+str(ii)+' && y1=1) -> X((x1='+str(ii+1)+' && y1=1)||(x1='+str(ii)+' && y1=1))'}#|| (x1='+str(ii+1)+' && y1=2))'}
+        tester_safe |= {'(x2='+str(ii)+' && y2=2) -> X((x2='+str(ii+1)+' && y2=2)||(x2='+str(ii)+' && y2=2))'}#|| (x2='+str(ii+1)+' && y2=1))'}
+        # tester_safe |= {'(x2='+str(ii)+' && y2=1) -> X((x2='+str(ii+1)+' && y2=1)||(x2='+str(ii)+' && y2=1))'}#|| (x2='+str(ii+1)+' && y2=2))'}
+        tester_safe |= {'(x1='+str(ii)+' && y1=2) -> X((x1='+str(ii+1)+' && y1=2)||(x1='+str(ii)+' && y1=2))'}#|| (x1='+str(ii+1)+' && y1=1))'}
+    # tester_safe |= {'!(x1='+str(tracklength)+' && x2=0)'}
+    tester_safe |= {'(x2='+str(tracklength-1)+' && y1=2) -> X(x2='+str(tracklength-1)+' && y1=2)'}
+    tester_safe |= {'(x1='+str(tracklength)+' && y1=2) -> X(x1='+str(tracklength)+' && y1=2)'}
+    # tester_safe |= {'(x1='+str(tracklength-1)+') -> X(x1='+str(tracklength-1)+' || x1='+str(tracklength)+')'}
+    # tester_safe |= {'(x2=0) -> X(x2=0 || x2=1)'}
+
+    # Terminal conditions: Once car merges, it remains merged.
+    # for xi in range(0,tracklength+1):
+    #     for x1i in range(1,tracklength):
+    #         for x2i in range(0,x1i):
+    #             other_st_i = '(x = '+str(xi)+') && (x1='+str(x1i)+')&&(x2='+str(x2i)+')'
+    #             sys_safe |= {'((y=2) && '+other_st_i+')-> X('+other_st_i+'&& (y=2))'}
+    #             tester_safe |= {'((y=2) && '+other_st_i+')-> X('+other_st_i+'&& (y=2))'}
+
+    # Synthesize specs
+    ego_spec = Spec(sys_vars, sys_init, sys_safe, sys_prog)
+    test_spec = Spec(tester_vars, tester_init, tester_safe, tester_prog)
+    # st()
+
+    return ego_spec, test_spec
+
 # System and environment have been switched:
 def specs_for_entire_track(tracklength):
+    # st()
     sys_vars = {}
     sys_vars['x'] = (0, tracklength) # 0: system is much behind the second tester car, 6: system is much further than the first tester car
     sys_vars['y'] = (1,2)
@@ -393,6 +555,7 @@ def specs_for_entire_track(tracklength):
     tester_prog = set()
     for ki in range(1,tracklength-1):
         tester_prog |= {'((x='+str(ki+1)+') && (x1='+str(ki+2)+') && (x2='+str(ki)+')) && (y=2 && y1=2 && y2=2)'}
+    tester_prog |= {'(y=2 && y1=2 && y2=2)'}
     tester_safe = set()
 
     # No collision with other vehicles:
@@ -433,6 +596,8 @@ def specs_for_entire_track(tracklength):
     # Synthesize specs
     ego_spec = Spec(sys_vars, sys_init, sys_safe, sys_prog)
     test_spec = Spec(tester_vars, tester_init, tester_safe, tester_prog)
+    # st()
+
     return ego_spec, test_spec
 
 def simple_test_specs():
@@ -571,18 +736,21 @@ if __name__ == '__main__':
         aut = example_win_set3()
 
     elif ex==5: # Constructing abstraction for the merge example
-        ego_spec, test_spec = specs_for_entire_track(10)
+        ego_spec, test_spec = spec_merge_in_front()#test_spec()#specs_for_entire_track(5)
         gr_spec = make_grspec(test_spec, ego_spec) # Placing test_spec as sys_spec and sys_spec as env_spec to
         # invert the tester and the system
+        print(gr_spec.pretty())
+        # print(test_spec.pretty())
         w_set = WinningSet()
         w_set.set_spec(gr_spec)
         aut = w_set.make_compatible_automaton(gr_spec)
 
     winning_set = w_set.find_winning_set(aut)
-    pdb.set_trace()
+    # ipdb.set_trace()
     # (x,y), (x1, y1), (x2,y2) are the positions of the system under test, the leading tester car, and the second tester car respectively. Domains of the position values can be found in the variable declarations in the specs_for_entire_track() function.
-    state = {'x': 1, 'y': 1, 'x1': 3, 'y1':2, 'x2':1, 'y2': 2}  # To check if a state is in the winning set, pass all values in dictionary form. Each dictionary corresponds to one state.
+    state = {'x': 1, 'y': 1, 'x1': 1}#, 'y1':2, 'x2':1, 'y2': 2}  # To check if a state is in the winning set, pass all values in dictionary form. Each dictionary corresponds to one state.
     check_bdd = w_set.check_state_in_winset(aut, winning_set, state) # Check-bdd is a boolean. True implies that state is in the winning set.
+    ipdb.set_trace()
     if check_bdd:
         print("State is in the winning set")
     else:
