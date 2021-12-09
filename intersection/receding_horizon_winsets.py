634d5620a14395c3d50a09f120fa5f8ec1e87578
Apurva <abadithe@caltech.edu>
2021-12-09 14:02:25 -0800

    receding_horizon_skeleton

--- intersection/receding_horizon_winsets.py
+++ intersection/receding_horizon_winsets.py
@@ -34,6 +34,7 @@ from abstraction_intersection import get_graph, create_intersection_from_file
 # This function returns a dictionary of the set of Wjs for each goal
 # Intuition: This is similar to specifying the terminal condition in MPC
 # Depending on states that satisfy the goal_lambda condition, those are added to the Wj_dict
+# Get Vj for all goals
 def get_Vj_for_all_goals(tracklength, G, st2ver_dict, ver2st_dict, state_tracker, goal_lambda):
     """
     Parameters
@@ -55,14 +56,13 @@ def get_Vj_for_all_goals(tracklength, G, st2ver_dict, ver2st_dict, state_tracker
     """
     Vj_dict = dict()
     goal_nodes = get_goal_states(G, goal_lambda, ver2st_dict, state_tracker)
-
     for i in goal_nodes:
-        Vj_dict[i] = get_Wj(tracklength, G, st2ver_dict, ver2st_dict, i)
+        Vj_dict[i] = get_Vj(tracklength, G, st2ver_dict, ver2st_dict, i)
     return Vj_dict
 
-# Function to get Wi_j, which is the set of all states that are j*horizon steps
+# Function to get Vi_j, which is the set of all states that are j*horizon steps
 # (1 step = 1 round of play) away from set from progress goal []<>i:
-def get_Wj(tracklength, G, st2ver_dict, ver2st_dict, goal):
+def get_Vj(tracklength, G, st2ver_dict, ver2st_dict, goal):
     """
     Parameters
     ----------
@@ -78,53 +78,36 @@ def get_Wj(tracklength, G, st2ver_dict, ver2st_dict, goal):
         DESCRIPTION.
     Returns
     -------
-    Wj_set : TYPE
+    Vj_set : TYPE
         DESCRIPTION.
     """
     # G_aux, target = add_aux_nodes(G, goal_state)
     node_dist = dict()
-    Wj_set = dict()
+    Vj_set = dict()
     for node in list(G.nodes):
         if nx.has_path(G, node, goal):
             pl = shortest_path_length(G, node, goal)
             node_dist[node] = pl
-            if pl not in list(Wj_set.keys()):
-                Wj_set[pl] = [node]
+            if pl not in list(Vj_set.keys()):
+                Vj_set[pl] = [node]
             else:
-                Wj_set[pl].append(node)
-    return Wj_set
+                Vj_set[pl].append(node)
+    return Vj_set
 
 
-# Ego safe controller for merging in between for specific tracklength and merge_setting:
-def get_ego_safety(tracklength, merge_setting):
+# Ego safe controller for left turn requirement:
+def get_ego_safety():
     ego_safe = set()
-
-    # Dynamics for merging into adjacent track:
-    for ii in range(1,tracklength):
-        ego_safe |= {'(x='+str(ii)+' && y=1) -> X((x='+str(ii+1)+' && y=1)||(x='+str(ii)+' && y=1)|| (x='+str(ii+1)+' && y=2))'}
-        ego_safe |= {'(x='+str(ii)+' && y=2) -> X((x='+str(ii+1)+' && y=2)||(x='+str(ii)+' && y=2)|| (x='+str(ii+1)+' && y=1))'}
-    ego_safe |= {'(x='+str(tracklength)+' && y=2 )-> X(x='+str(tracklength)+' && y=2)'}
-    ego_safe |= {'(x='+str(tracklength)+' && y=1 )-> X(x='+str(tracklength)+' && y=1)'}
-
-    # If there is no env car in the kitty-corner lane, the car merges:
-    for xi in range(1, tracklength-1):
-        merge_if_possible = '(!(y1=2 && x1 = '+str(xi+1) +') && !(y2=2 && x2 = '+str(xi+1) +')) -> X(y=2 && x='+str(xi+1)+')'
-        ego_safe |= {merge_if_possible}
-
-    # Last cell merge:
-    merge_if_possible = '(!(y1=2 && x1 = '+str(tracklength) +')) -> X(y=2 && x='+str(tracklength)+')'
-    ego_safe |= {merge_if_possible}
-
-    # Don't start until x1 is in front:
-    ego_safe |= {'(x1=2 && y1=2) -> X(x=1 && y=1)'}
+    # Fill in ego safety specifications:
     return ego_safe
 
-# Function to add progress properties of the jth specification for the ith goal:
-# Keep separate goals for each winning set
-# i: goal
-# Vij_dict: Dictionary
-# j: Distance from the goal
-def add_psi_i_j_progress(Vij_dict, j, ver2st_dict, state_tracker):
+def add_psi_i_j_progress(Vij_dict, j, ver2st_dict):
+    '''
+    Function to add progress properties of the jth specification for the ith goal. Keep separate goals for each winning set.
+    Vij_dict: Set of all Vjs for goal i
+    j: Distance to goal i
+    ver2st_dict: Dictionary going from number to state dictionary
+    '''
     assumption = set()
     prog_guarantee = set()
     Vj = Vij_dict[j]
@@ -142,8 +125,10 @@ def add_psi_i_j_progress(Vij_dict, j, ver2st_dict, state_tracker):
 
     return assumption, prog_guarantee, FVj
 
-# Define string for state specification:
 def get_str_spec(state_dict):
+    '''
+    Helper function that define string from state_dict
+    '''
     spec_state = ""
     for k, v in state_dict.items():
         if spec_state == "":
@@ -153,8 +138,10 @@ def get_str_spec(state_dict):
     spec_state += ")"
     return spec_state
 
-# Function to construct set membership:
 def construct_spec_set_membership(Vj, ver2st_dict):
+    '''
+    Helper function that constructs set membership
+    '''
     spec = "("
     for vj in Vj:
         state_dict = ver2st_dict[vj]
@@ -165,15 +152,12 @@ def construct_spec_set_membership(Vj, ver2st_dict):
     spec += ")"
     return spec
 
-# Safety specifications for the test agent:
-def get_test_safety(tracklength, merge_setting):
+def get_test_safety():
     '''
+    Function to specify safety specifications for the test environments
     Parameters
     ----------
-    tracklength : int
-        Length of the road.
-    merge_setting : str
-        set to one of: between, front or back.
+
     Returns
     -------
     test_safe : set(str)
@@ -181,49 +165,27 @@ def get_test_safety(tracklength, merge_setting):
         This function returns the dynamics of the test agents
     '''
     test_safe = set()
-    for ii in range(2,tracklength-1):
-        test_safe |= {'(x1='+str(ii)+' && y1=1) -> X((x1='+str(ii+1)+' && y1=1)||(x1='+str(ii)+' && y1=1)|| (x1='+str(ii+1)+' && y1=2))'}
-        test_safe |= {'(x2='+str(ii)+' && y2=2) -> X((x2='+str(ii+1)+' && y2=2)||(x2='+str(ii)+' && y2=2)|| (x2='+str(ii+1)+' && y2=1))'}
-        test_safe |= {'(x2='+str(ii)+' && y2=1) -> X((x2='+str(ii+1)+' && y2=1)||(x2='+str(ii)+' && y2=1)|| (x2='+str(ii+1)+' && y2=2))'}
-        test_safe |= {'(x1='+str(ii)+' && y1=2) -> X((x1='+str(ii+1)+' && y1=2)||(x1='+str(ii)+' && y1=2)|| (x1='+str(ii+1)+' && y1=1))'}
-    test_safe |= {'(x2='+str(tracklength)+' && y2=2) -> X(x2='+str(tracklength)+' && y2=2)'}
-    test_safe |= {'(x2='+str(tracklength)+' && y2=1) -> X(x2='+str(tracklength)+' && y2=1)'}
-
-    test_safe |= {'(x2=1 && y2=2) -> X((x2=1 && y2=2) || (x2=2 && y2=2) || (x2=2 && y2=1))'}
-    test_safe |= {'(x2=1 && y2=1) -> X((x2=1 && y2=1) || (x2=2 && y2=2) || (x2=2 && y2=1))'}
-
-    test_safe |= {'(x1='+str(tracklength)+' && y1=2) -> X(x1='+str(tracklength)+' && y1=2)'}
-    test_safe |= {'(x1='+str(tracklength)+' && y1=1) -> X(x1='+str(tracklength)+' && y1=1)'}
-
-    test_safe |= {'(x1='+str(tracklength-1)+' && y1=1) -> X((x1='+str(tracklength-1)+' && y1=1)|| (x1='+str(tracklength)+' && y1=2) || (x1='+str(tracklength)+' && y1=1)) '}
-    test_safe |= {'(x1='+str(tracklength-1)+' && y1=2) -> X((x1='+str(tracklength-1)+' && y1=2)|| (x1='+str(tracklength)+' && y1=1) || (x1='+str(tracklength)+' && y1=2))'}
     return test_safe
 
 # Function to add merge specifications:
-def add_merge_specs(tracklength, merge_setting, test_safe):
+def add_goal_specs(test_safe):
     """
+    Goal specifications for the intersection example
     Parameters
     ----------
-    tracklength : TYPE
-        DESCRIPTION.
-    merge_setting : TYPE
-        DESCRIPTION.
-    test_safe : TYPE
+    test_safe : Set of initial safety specifications
         DESCRIPTION.
     Returns
     -------
-    test_safe : TYPE
+    test_safe : Set of safety specifications with goal requirements added in
         DESCRIPTION.
     """
-    merge_spec = '((x=2 && y=2) -> (x1=3 && x2=1 && y1=2 && y2=2))'
-    for ki in range(2,tracklength-1):
-        merge_spec = merge_spec + ' || ((x='+str(ki+1)+' && y=2) -> ((x1='+str(ki+2)+' && (x2='+str(ki)+') && y1=2 && y2=2)))'
-    test_safe |= {merge_spec}
+    goal_spec = ''
+    test_safe |= {goal_spec}
     return test_safe
 
 # Function to get all possible states in the graph of all system and environment transitions:
-def specs_car_rh(tracklength, merge_setting):
-    # tracklength = 10
+def specs_car_rh():
     ego_vars = {}
     ego_vars['x'] = (1, tracklength)
     ego_vars['y'] = (1,2)
