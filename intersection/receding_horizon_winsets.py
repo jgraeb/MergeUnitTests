59c9c426ebb1a00e869522f5b3f8a899a56193a4
Apurva <abadithe@caltech.edu>
2021-12-09 15:36:03 -0800

    add_specs

--- intersection/receding_horizon_winsets.py
+++ intersection/receding_horizon_winsets.py
@@ -187,32 +187,31 @@ def add_goal_specs(test_safe):
 # Function to get all possible states in the graph of all system and environment transitions:
 def specs_car_rh():
     ego_vars = {}
-    ego_vars['x'] = (1, tracklength)
-    ego_vars['y'] = (1,2)
-    ego_init = {'x='+str(1), 'y='+str(1)}
-    ego_safe = get_ego_safety(tracklength, merge_setting)
-    ego_prog = {'y=2'}
+    ego_vars['y'] = (0,7)
+    ego_vars['z'] = (0,7)
+    ego_init = {'y='+str(1), 'z='+str(1)}
+    ego_safe = get_ego_safety()
+    ego_prog = {'z=2'}
 
     test_vars = {}
-    test_vars['x1'] = (1, tracklength)
-    test_vars['x2'] = (1, tracklength)
-    test_vars['y1'] = (1,2)
-    test_vars['y2'] = (1,2)
-    test_init = {'x1='+str(2), 'y1='+str(2), 'x2='+str(1), 'y2='+str(2)}
+    test_vars['y1'] = (0,7)
+    test_vars['y2'] = (0,7)
+    test_vars['z1'] = (0,7)
+    test_vars['z2'] = (0,7)
+    test_init = {'y1='+str(0), 'y2='+str(0), 'z1='+str(7), 'z2='+str(7)}
     test_safe = set()
-    test_safe = get_test_safety(tracklength, merge_setting)
-    test_safe = add_merge_specs(tracklength, merge_setting, test_safe)
+    test_safe = get_test_safety()
+    test_safe = add_goal_specs(test_safe)
     test_prog = set()
     # test_prog = add_psi_j_progress(tracklength, j, merge_setting)
 
-    ego_n = 2*tracklength
-    tester_n = (tracklength-1)**2
-    G, st2ver_dict, ver2st_dict, state_tracker, state_dict_test, state_dict_system = get_all_states(tracklength, ego_n, tester_n)
-    merge_setting = "between"
-    # pdb.set_trace()
-    goal_lambda = construct_lambda_function(merge_setting)
+    ego_n = ... # Fill in number of ego states
+    tester_n = ... # Fill in number of tester states
+    G, st2ver_dict, ver2st_dict, state_tracker, state_dict_test, state_dict_system = get_all_states(ego_n, tester_n)
+
+
+    goal_lambda = construct_lambda_function()
     Vij_dict = get_Vj_for_all_goals(tracklength, G, st2ver_dict, ver2st_dict, state_tracker, goal_lambda)
-    # pdb.set_trace()
 
     ego_spec = Spec(ego_vars, ego_init, ego_safe, ego_prog)
     test_spec = Spec(test_vars, test_init, test_safe, test_prog)
@@ -220,7 +219,7 @@ def specs_car_rh():
 
 # Function to get all receding horizon winning sets:
 # tracklength: length of the track; merge_setting: between/ in front/ behind
-def get_winset_rh(tracklength, merge_setting, Vij, state_tracker, ver2st_dict,ego_spec_orginal, test_spec_orginal, state_test_dict, state_system_dict, G):
+def get_winset_rh(Vij, state_tracker, ver2st_dict,ego_spec_orginal, test_spec_orginal, state_test_dict, state_system_dict, G):
     # Get Wij_dict: The list of Vij
     # Existing safety, progress and init properties
     # ego_spec, test_spec, Vij_dict, state_tracker, ver2st_dict = specs_car_rh(tracklength, merge_setting)
@@ -255,24 +254,21 @@ def get_winset_rh(tracklength, merge_setting, Vij, state_tracker, ver2st_dict,eg
 
             aut = w_set.make_compatible_automaton(gr_spec)
             # g = synthesize_some_controller(aut)
-            agentlist = ['x1', 'x2']
+            agentlist = ['y1', 'y2']
             fp = w_set.find_winning_set(aut)
-            # print("Printing states in fixpoint: ")
-            # pdb.set_trace()
-            states_in_fp, states_out_fp = check_all_states_in_fp(tracklength, agentlist, w_set, fp, aut)
+            states_in_fp, states_out_fp = check_all_states_in_fp(agentlist, w_set, fp, aut)
             if PRINT_STATES_IN_COMPUTATION:
                 print(" ")
                 print("Printing states in winning set: ")
             # Filter out states that begin in Vj, are in the fixpoint, and satisfy the assumptions
             start_set = Vij[j]
 
-            states_in_winset, states_out_winset = check_all_states_in_winset_rh(tracklength, agentlist, w_set, fp, aut, merge_setting, state_test_dict, state_system_dict, goal_states, G, ver2st_dict, start_set)
-            # st()
+            states_in_winset, states_out_winset = check_all_states_in_winset_rh(agentlist, w_set, fp, aut, state_test_dict, state_system_dict, goal_states, G, ver2st_dict, start_set)
             Wij.update({j: states_in_winset})
     # st()
     return Wij
 
-def get_tester_states_in_winsets(tracklength, merge_setting):
+def get_tester_states_in_winsets():
     """
     Find all tester states in the winning sets.
     W - Winning set
@@ -281,12 +277,12 @@ def get_tester_states_in_winsets(tracklength, merge_setting):
     j - distance to corresponding goal in steps
     k - index of the distance to the goal in j-1
     """
-    ego_spec, test_spec, Vij_dict, state_tracker, ver2st_dict, G, state_dict_test, state_dict_system = specs_car_rh(tracklength, merge_setting)
+    ego_spec, test_spec, Vij_dict, state_tracker, ver2st_dict, G, state_dict_test, state_dict_system = specs_car_rh()
     Wij = dict()
     # Go through all the goal states
     # st()
     for key in Vij_dict.keys():
-        Wj = get_winset_rh(tracklength, merge_setting, Vij_dict[key], state_tracker, ver2st_dict,ego_spec, test_spec, state_dict_test, state_dict_system, G)
+        Wj = get_winset_rh(Vij_dict[key], state_tracker, ver2st_dict,ego_spec, test_spec, state_dict_test, state_dict_system, G)
         Wij.update({key: Wj})
     # st()
     # Now find all the ks in each Wij
@@ -336,14 +332,12 @@ def get_tester_states_in_winsets(tracklength, merge_setting):
     """
     return Wij, Vij_dict, state_tracker, ver2st_dict
 
-def check_if_state_in_winset(state):
-    pass
-
 
 def check_system_states_in_winset(origin_state, state, ver2st_dict, state_tracker, Wij, debug = False):
-    # st()
-    if origin_state == {'x': 4, 'y': 1, 'x1': 5, 'y1': 2, 'x2': 3, 'y2': 2}:
-        st()
+    '''
+    Function to check if system states are in the winning set.
+    '''
+
     next_states = find_next_states(state, ver2st_dict, state_tracker)
 
     # Find the next states in vertex represenation - probably not necessary
@@ -354,8 +348,6 @@ def check_system_states_in_winset(origin_state, state, ver2st_dict, state_tracke
             if state_tracker[vertex] == 't':
                 next_states_ver.append(vertex)
 
-    # Find if all of these states are in Wij
-    # st()
     # Find j or original state for each i and compare to that value for all new states
     progress = False
     for i in Wij:
@@ -393,42 +385,30 @@ def check_system_states_in_winset(origin_state, state, ver2st_dict, state_tracke
 
 def find_next_states(state, ver2st_dict, state_tracker):
     """
-    Propagate forward to check if the system state is in the winning set.
+    Propagate forward to check if the system state is in the winning set. Find next states from the current state.
     """
     # find sys state in dict form if given as vertex
     if not isinstance(state,dict):
         state = ver2st_dict[state]
-    x = state['x']
-    y = state['y']
-    tester_states = [(state['x1'],state['y1']),(state['x2'],state['y2'])]
+    x = state['y']
+    y = state['z']
+    tester_states = [(state['y1'],state['z1']),(state['y2'],state['z2'])]
     # feed in system state and simulate all possible next states
-    # st()
     next_states = []
-    actions =  {'move': (1,0), 'stay': (0,0), 'merge': (1,1)}
-    # check for merge
-    move_x, move_y = actions['merge']
-    act_x = x + move_x
-    act_y = y + move_y
-    if (act_x,act_y) not in tester_states:
-        # return the next state as merged
-        state_dict_form = make_dict_form((act_x,act_y), tester_states)
-        next_states.append(state_dict_form)
-        return next_states
-    else:
-        for action in actions.keys():
-            move_x, move_y = actions[action]
-            act_x = x + move_x
-            act_y = y + move_y
-            if (act_x,act_y) not in tester_states:
-                state_dict_form = make_dict_form((act_x,act_y), tester_states)
-                next_states.append(state_dict_form)
-        return next_states
+    actions =  {} # Actions
+    return next_states
 
 def make_dict_form(sys_state, tester_states):
+    '''
+    Make dictionary form of sys_state and tester_states
+    '''
     state_dict = {'x': sys_state[0], 'y': sys_state[1], 'x1': tester_states[0][0], 'y1':tester_states[0][1], 'x2': tester_states[1][0], 'y2': tester_states[1][1]}
     return state_dict
 
 def get_dict_inv_multiple(dict_in, value):
+    '''
+    Get the dictionary representation of state and getting the key value in terms of state
+    '''
     keys = []
     for k, v in dict_in.items():
         if v == value:
